4a5155bf2f207e7b85f49312cfa1a464d90a62f2
cher1shRXD <taewoo080401@gmail.com>
2024-12-19 15:52:14 +0900

    feat: folder structure

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+export const COLOR = {
+  100: "#b9e4da",
+  200: "#9bd9cb",
+  300: "#7ecebb",
+  400: "#60c3ac",
+  500: "#45b59b",
+  600: "#3a9882",
+  700: "#2e7a68",
+  800: "#235c4f",
+  900: "#183f36",
+  backgroundColor: "#1b1b1b",
+  boxColor: "#202020",
+  itemColor: "#3b3b3b",
+  borderColor: "#464646",
+};
